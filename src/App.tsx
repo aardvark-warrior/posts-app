5f57ef815e286077cb418c655e28218dadcc2485
Arthur Wang <75041710+aardvark-warrior@users.noreply.github.com>
2024-02-02 13:38:00 -0500

    Installed and configured tslog

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Button } from "./components/ui/button";
+import { log } from "./lib/logger"; 
 
 function App() {
+  log.debug("Hello world");
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Button>Hello, world!</Button>
